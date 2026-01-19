0745083aba7df9ed0fa81d53f2cba9faad82271a
eneging <140635337+eneging@users.noreply.github.com>
2026-01-19 09:53:34 -0500

    frondtend

--- src/app/components/BottomNav.tsx
+++ src/app/components/BottomNav.tsx
@@ -1,17 +1,17 @@
 "use client";
 
 import Link from "next/link";
-import { Home, Search, ReceiptText, BadgePercent, Store
+import { Home, ReceiptText, BadgePercent, Store
      //Layers
      } from "lucide-react";
 
 export default function BottomNav() {
   return (
-    <nav className="fixed bottom-0 left-0 right-0 bg-black  md:hidden z-50">
+    <nav className="fixed bottom-0 left-0 right-0 bg-orange-500 rounded-t-3xl md:hidden z-50">
       <div className="flex justify-around items-center h-16 px-2">
         <Link
           href="/"
-          className="flex flex-col items-center text-gray-100 hover:text-blue-400 transition"
+          className="flex flex-col items-center text-gray-100 hover:text-black transition"
         >
           <Home className="w-5 h-5" />
           <span className="text-[11px]">Inicio</span>
@@ -19,7 +19,7 @@ export default function BottomNav() {
 
         <Link
           href="/search"
-          className="flex flex-col items-center text-gray-100 hover:text-blue-400 transition"
+          className="flex flex-col items-center text-gray-100 hover:text-black transition"
         >
          <Store className=" w-5 h-5"/>
           <span className="text-[11px]">Licoreria</span>
@@ -27,7 +27,7 @@ export default function BottomNav() {
 
         <Link
           href="/promociones"
-          className="flex flex-col items-center text-gray-100 hover:text-blue-400 transition"
+          className="flex flex-col items-center text-gray-100 hover:text-black transition"
         >
             <BadgePercent className=" w-5 h-5 " />
 
